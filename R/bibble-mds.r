5906cc89d617dd8628446fb156543ac86f335ef1
Jason Cory Brunson <cornelioid@gmail.com>
2018-04-27 16:22:04 -0400

    switch from 'dimension' to 'matrix' argument in 'get_uv' mds functions

--- R/bibble-mds.r
+++ R/bibble-mds.r
@@ -9,16 +9,16 @@ as_bibble.cmds <- function(x) {
   x
 }
 
-get_uv_cmds <- function(x, dimension) {
-  name_fun <- switch(dimension, `1` = rownames, `2` = colnames)
+get_uv_cmds <- function(x, matrix) {
+  name_fun <- switch(matrix, u = rownames, v = colnames)
   x$points %>%
     as_tibble() %>%
-    mutate(name = name_fun(x$x)) %>%
+    {if (is.null(name_fun(x$x))) . else mutate(., name = name_fun(x$x))} %>%
     rename_at(vars(matches("^V[1-9]+$")), funs(gsub("^V", "PCo", .))) %>%
     select(starts_with("PCo"), everything())
 }
-get_u.cmds <- function(x) get_uv_cmds(x, 1)
-get_v.cmds <- function(x) get_uv_cmds(x, 2)
+get_u.cmds <- function(x) get_uv_cmds(x, "u")
+get_v.cmds <- function(x) get_uv_cmds(x, "v")
 get_coordinates.cmds <- function(x) {
   tibble(
     .name = paste0("PCo", 1:ncol(x$points)),
