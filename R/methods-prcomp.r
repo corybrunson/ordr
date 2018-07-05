e2f452a1e182a8c41de51cf80a34c9d098468a77
Emily Paul <erpb.71@gmail.com>
2018-07-05 14:26:10 -0400

    accessor method for rotated data

--- R/methods-prcomp.r
+++ R/methods-prcomp.r
@@ -1,2 +1,11 @@
-#' Functionality for prcomp
+#' Functionality for prcomp objects
 #' 
+
+get_rotated_data <- function(obj){
+  if (class(obj) != "prcomp"){
+    stop("get_rotated_data() can only be called on an object of class prcomp")
+  }
+  else {
+    obj$x
+  }
+}
