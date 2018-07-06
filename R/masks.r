ddb96790ad11c74664803fd9687cb287fa584dd8
corybrunson <jabrunso@vt.edu>
2018-07-06 16:35:54 -0400

    mask convexLogisticPCA

--- R/masks.r
+++ R/masks.r
@@ -12,6 +12,7 @@
 #' @inheritParams stats::cmdscale
 #' @inheritParams logisticPCA::logisticPCA
 #' @inheritParams logisticPCA::logisticSVD
+#' @inheritParams logisticPCA::convexLogisticPCA
 #' @param ... Additional parameters passed to original functions.
 
 #' @rdname masks
@@ -20,6 +21,27 @@ cmdscale <- function(d, k = 2, add = FALSE) {
   res <- stats::cmdscale(d, k = k, eig = TRUE, add = add, x.ret = TRUE)
   class(res) <- "cmds"
   res
+}
+
+#' @rdname masks
+#' @export
+logisticSVD <- function(
+  x, k = 2,
+  quiet = TRUE, max_iters = 1000,
+  conv_criteria = 1e-05, random_start = FALSE,
+  ...,
+  partial_decomp = TRUE, main_effects = TRUE
+) {
+  lsvd <- logisticPCA::logisticSVD(
+    x = x, k = k,
+    quiet = quiet, max_iters = max_iters,
+    conv_criteria = conv_criteria, random_start = random_start,
+    ...,
+    partial_decomp = partial_decomp, main_effects = main_effects
+  )
+  rownames(lsvd$A) <- rownames(x)
+  rownames(lsvd$B) <- colnames(x)
+  lsvd
 }
 
 #' @rdname masks
@@ -47,21 +69,23 @@ logisticPCA <- function(
 
 #' @rdname masks
 #' @export
-logisticSVD <- function(
-  x, k = 2,
-  quiet = TRUE, max_iters = 1000,
-  conv_criteria = 1e-05, random_start = FALSE,
+convexLogisticPCA <- function(
+  x, k = 2, m = 4,
+  quiet = TRUE, partial_decomp = FALSE,
+  max_iters = 1000, conv_criteria = 1e-06,
+  random_start = FALSE,
   ...,
-  partial_decomp = TRUE, main_effects = TRUE
+  main_effects = TRUE, ss_factor = 4
 ) {
-  lsvd <- logisticPCA::logisticSVD(
-    x = x, k = k,
-    quiet = quiet, max_iters = max_iters,
-    conv_criteria = conv_criteria, random_start = random_start,
+  lpca <- logisticPCA::convexLogisticPCA(
+    x, k = k,
+    m = m, quiet = quiet, partial_decomp = partial_decomp,
+    max_iters = max_iters, conv_criteria = conv_criteria,
+    random_start = random_start,
     ...,
-    partial_decomp = partial_decomp, main_effects = main_effects
+    main_effects = main_effects, ss_factor = 4
   )
-  rownames(lsvd$A) <- rownames(x)
-  rownames(lsvd$B) <- colnames(x)
-  lsvd
+  rownames(lpca$U) <- colnames(x)
+  #rownames(lpca$PCs) <- rownames(x)
+  lpca
 }
