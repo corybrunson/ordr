6b5deea549a5487fb0a9a149d5e40f26ebd4eed0
corybrunson <jabrunso@vt.edu>
2018-07-08 16:48:45 -0500

    omit negative eigenvalues from tbl_ord presentation

--- R/methods-eigen.r
+++ R/methods-eigen.r
@@ -24,7 +24,9 @@ as_tbl_ord.eigen <- as_tbl_ord_default
 
 recover_uv_eigen <- function(x, .matrix) {
   .matrix <- match_factor(.matrix)
-  res <- x$vectors %*% diag(sqrt(x$values), nrow = length(x$values))
+  w <- 1:(min(which(c(x$values, -1) < 0)) - 1)
+  res <- x$vectors[, w, drop = FALSE] %*%
+    diag(sqrt(x$values[w]), nrow = length(w))
   #res <- sweep(x$vectors, 2, sqrt(x$values), "*")
   dimnames(res) <- list(
     dimnames(attr(x, "x"))[[switch(.matrix, u = 1, v = 2)]],
@@ -43,7 +45,10 @@ recover_v.eigen <- function(x) recover_uv_eigen(x, "v")
 
 #' @rdname methods-eigen
 #' @export
-recover_coord.eigen <- function(x) paste0("E", 1:ncol(x$vectors))
+recover_coord.eigen <- function(x) {
+  w <- 1:(min(which(c(x$values, -1) < 0)) - 1)
+  paste0("E", w)
+}
 
 #' @rdname methods-eigen
 #' @export
@@ -72,9 +77,10 @@ augment_v.eigen <- function(x) {
 #' @rdname methods-eigen
 #' @export
 augment_coord.eigen <- function(x) {
+  w <- 1:(min(which(c(x$values, -1) < 0)) - 1)
   tibble(
     .name = recover_coord(x),
-    .values = x$values[1:ncol(x$vectors)]
+    .values = x$values[w]
   )
 }
 
@@ -85,7 +91,7 @@ negate_to.eigen <- function(x, y, ..., .matrix) {
   # get negations
   s <- negation_to(get_factor(as_tbl_ord(x), .matrix), y)
   # tag 'eigen' object with negation
-  x <- attribute_alignment(x, diag(s, nrow = ncol(x$vectors)))
+  x <- attribute_alignment(x, diag(s, nrow = min(which(c(x$values, -1) < 0)) - 1))
   # return annotated object
   x
 }
