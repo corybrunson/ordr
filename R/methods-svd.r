ff0ab6ddb9eafe180ff4e3e1b43190e9a7debd83
Emily Paul <erpb.71@gmail.com>
2018-07-09 12:57:00 -0400

    accessor method for diagonal matrix

--- R/methods-svd.r
+++ R/methods-svd.r
@@ -9,3 +9,12 @@ get_singular_values <- function(x){
     x$d
   }
 }
+
+get_diagonal_matrix <- function(x){
+  if (all(names(x) != t(c("d", "u", "v")))){
+    stop("get_diagonal_matrix() can only be called on a singular value decomposition output")
+  }
+  else {
+    diag(x$d)
+  }
+}
