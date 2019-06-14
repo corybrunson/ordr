19562bf90aa19b51b905813717b36489eee391cc
corybrunson <jabrunso@vt.edu>
2019-06-13 23:59:12 -0400

    unit circle geom layer

---
+++ R/geom-unit-circle.r
@@ -0,0 +1,82 @@
+#' @title Render a unit circle
+#' 
+
+#' @description `geom_unit_circle()` renders the unit circle, centered at the
+#'   origin with radius 1.
+#' @template ggbiplot-layers
+
+#' @section Aesthetics:
+
+#' `geom_unit_circle()` understands the following aesthetics (none required):
+
+#' - `alpha`
+#' - `colour`
+#' - `linetype`
+#' - `size`
+#' 
+
+#' @name ggbiplot-unit-circle
+#' @import ggplot2
+#' @inheritParams ggplot2::layer
+#' @template param-geom
+#' @param segments The number of segments to be used in drawing the circle.
+NULL
+
+#' @rdname ggbiplot-unit-circle
+#' @usage NULL
+#' @export
+GeomUnitCircle <- ggproto(
+  "GeomUnitCircle", GeomPath,
+  
+  required_aes = c(),
+  default_aes = aes(colour = "black", size = 0.5, linetype = 1, alpha = NA),
+  
+  draw_panel = function(
+    data, panel_params, coord,
+    segments = 60
+  ) {
+    # first row (for aesthetics)
+    first_row <- data[1, setdiff(names(data), c("x", "y")), drop = FALSE]
+    rownames(first_row) <- NULL
+    
+    # unit circle as a path
+    angles <- (0:segments) * 2 * pi/segments
+    unit_circle <- data.frame(x = cos(angles), y = sin(angles), group = 1)
+    
+    # data frame of segments with aesthetics
+    data <- cbind(unit_circle, first_row)
+    
+    # return unit circle grob
+    GeomPath$draw_panel(
+      data = data, panel_params = panel_params, coord = coord,
+      na.rm = FALSE
+    )
+  },
+  
+  draw_key = draw_key_blank
+)
+
+#' @rdname ggbiplot-unit-circle
+#' @export
+geom_unit_circle <- function(
+  mapping = NULL, data = NULL, stat = "identity", position = "identity",
+  segments = 60,
+  ...,
+  na.rm = FALSE,
+  show.legend = NA, inherit.aes = TRUE
+) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomUnitCircle,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      segments = segments,
+      ...
+    )
+  )
+}
