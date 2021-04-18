f6174538a4d7113a490b669778f0385cc950e4d6
Brunson <jason.brunson@FMSB-M437-W1.local>
2021-04-18 16:00:45 -0400

    doc(vignette): adapt 1st half of cmds vignette + add qswur data

--- R/data.r
+++ R/data.r
@@ -8,3 +8,21 @@
 #' @name glass
 #' @usage data(glass)
 NULL
+
+#' @title U.S. university rankings
+#'
+#' @description Classifications and rankings of U.S. universities for the years
+#'   2017--2020.
+#'
+#' @details
+#'
+#' **Link to the websites
+#' <http://www.iu.qs.com/university-rankings/qs-classifications/> for university
+#' classifications.**
+#'
+#' @format A [tibble][tibble::tibble].
+#' @keywords datasets
+#' @source Quacquarelli Symonds (2021).
+#' @name qswur_usa
+#' @usage data(qswur_usa)
+NULL
