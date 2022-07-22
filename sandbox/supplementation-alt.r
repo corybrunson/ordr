b7ef4af8a895d49301d3c483fbb4e3bcd46ee68e
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2022-07-22 08:44:48 -0400

    rm corrplot dependency + add alt supplementation idea to sandbox

---
+++ sandbox/supplementation-alt.r
@@ -0,0 +1,16 @@
+
+#' @rdname supplementation
+#' @export
+recover_supp_rows <- function(x) UseMethod("recover_supp_rows")
+
+#' @rdname supplementation
+#' @export
+recover_supp_cols <- function(x) UseMethod("recover_supp_cols")
+
+#' @rdname supplementation
+#' @export
+augmentation_supp_rows <- function(x) UseMethod("augmentation_supp_rows")
+
+#' @rdname supplementation
+#' @export
+augmentation_supp_cols <- function(x) UseMethod("augmentation_supp_cols")
