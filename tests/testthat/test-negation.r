2e4a4a0289bb4b6d8bd4b77e831a0b0be05775e9
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2022-08-13 12:56:08 -0400

    debug negation test

--- tests/testthat/test-negation.r
+++ tests/testthat/test-negation.r
@@ -1,11 +1,9 @@
 iris_pca <- as_tbl_ord(prcomp(iris[, 1:4]))
 
 test_that("negation is consistent via different input formats", {
-  expect_equal(
-    get_negation(negate_ord(iris_pca, c(2, 3))),
-    get_negation(negate_ord(iris_pca, c("PC2", "PC3"))),
-    get_negation(negate_ord(iris_pca, c(1, -1, -1, 1)))
-  )
+  res <- get_negation(negate_ord(iris_pca, c(2, 3)))
+  expect_equal(res, get_negation(negate_ord(iris_pca, c("PC2", "PC3"))))
+  expect_equal(res, get_negation(negate_ord(iris_pca, c(1, -1, -1, 1))))
 })
 
 test_that("negated ordinations preserve coordinate names", {
