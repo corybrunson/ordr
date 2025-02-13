2dd914c23d6eae8076456aa2c10a0e6e42dfc045
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2025-02-13 09:00:50 -0500

    write unit tests for vector geom

--- inst/examples/ex-geom-vector.r
+++ inst/examples/ex-geom-vector.r
@@ -1,8 +1,26 @@
-us_center <- sapply(state.center, \(x) (min(x) + max(x)) / 2)
-state_center <- cbind(
-  state = state.abb,
-  sweep(as.data.frame(state.center), 2, us_center, "-")
-)
-ggplot(state_center, aes(x, y, label = state)) +
-  coord_equal() +
-  geom_vector()
+# multidimensional scaling of covariances
+ability.cov$cov |> 
+  cov2cor() |>
+  eigen() |> getElement("vectors") |> 
+  as.data.frame() |> 
+  transform(test = rownames(ability.cov$cov)) ->
+  ability_cor_eigen
+ability_cor_eigen |> 
+  ggplot(aes(-V1, V2, label = test)) +
+  coord_square() + theme_void() +
+  geom_vector(check_overlap = TRUE) +
+  scale_y_continuous(expand = expansion(mult = .2)) +
+  ggtitle("Ability and intelligence test covariances")
+# multidimensional scaling of correlations
+ability.cov$cov |> 
+  eigen() |> getElement("vectors") |> 
+  as.data.frame() |> 
+  transform(test = rownames(ability.cov$cov)) ->
+  ability_cor_eigen
+ability_cor_eigen |> 
+  ggplot(aes(-V1, -V2, label = test)) +
+  coord_square() + theme_void() +
+  geom_vector(check_overlap = TRUE) +
+  geom_unit_circle() +
+  expand_limits(x = c(-1, 1), y = c(-1, 1)) +
+  ggtitle("Ability and intelligence test covariances")
