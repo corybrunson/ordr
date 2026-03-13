3508369ab36e98e9b43decd1b1e9e913b5308e37
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2026-03-13 12:56:46 -0400

    update documentation for elements param per deprecation of 'all' option

--- man-roxygen/param-elements.r
+++ man-roxygen/param-elements.r
@@ -1,5 +1,5 @@
 #' @param elements Character vector; which elements of each factor for which to
-#'   render graphical elements. One of `"all"` (the default), `"active"`, or any
+#'   render graphical elements. One of `"active"` (the default) or any
 #'   supplementary element type defined by the specific class methods (e.g.
 #'   `"score"` for 'factanal', 'lda_ord', and 'cancord_ord' and `"intraset"` and
-#'   `"interset"` for 'cancor_ord').
+#'   `"interset"` for 'cancor_ord'), via [partial matching][base::match.arg()].
