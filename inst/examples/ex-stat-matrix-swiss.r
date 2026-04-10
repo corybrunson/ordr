cb6b6b0d35f649ac99da48ab1451cb6c534edf7a
Jason Cory Brunson <cornelioid@gmail.com>
2026-04-10 16:45:54 -0400

    default data = NULL in matrix stat formals & add example & test - fixes #87

--- inst/examples/ex-stat-matrix-swiss.r
+++ inst/examples/ex-stat-matrix-swiss.r
@@ -5,7 +5,12 @@ swiss_fa <-
 get_rows(swiss_fa, elements = "active")
 head(get_rows(swiss_fa, elements = "score"))
 
-# biplot using element filters and selection
+# biplot using matrix stats and element filter
+ggbiplot(swiss_fa) +
+  stat_rows(elements = "score") +
+  stat_cols(geom = "vector", aes(label = name))
+
+# biplot using element filter and item selection
 # (note that filter precedes selection)
 ggbiplot(swiss_fa) +
   geom_rows_point(elements = "score") +
